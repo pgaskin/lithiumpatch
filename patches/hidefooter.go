026cc4d8bfe20f71d041a23a1849f92e18b8d393
Tuxx <tuxx@danktank.nl>
2025-09-22 05:12:11 +0200

    Bug: Header bar does not disappear in reading mode
    Fixed hiding of the header bar in reading mode when this patch is
    enabled.

--- patches/hidefooter.go
+++ patches/hidefooter.go
@@ -25,17 +25,31 @@ func init() {
 			InMethod("setChromeVisible(Z)V",
 				// increase locals to have spare temp registers v6,v7,v8
 				ReplaceString(".locals 6", ".locals 10"),
+				// Force nav bar translationY path to hidden on show when enabled
 				ReplaceStringPrepend(
 					"    if-eqz p1, :cond_3\n",
-					"    # lithiumpatch: hide footer bar if user enabled setting (only affects nav bar)\n"+
+					"    # lithiumpatch: hide footer bar on show when enabled (translationY)\n"+
 						"    const/4 v8, 0x0\n"+
 						"    iget-object v7, p0, Lcom/faultexception/reader/ReaderActivity;->mPrefs:Landroid/content/SharedPreferences;\n"+
 						"    const-string v6, \"hide_reader_footer\"\n"+
 						"    invoke-interface {v7, v6, v8}, Landroid/content/SharedPreferences;->getBoolean(Ljava/lang/String;Z)Z\n"+
 						"    move-result v8\n"+
-						"    if-eqz v8, :lith_patch_hf_cont\n"+
-						"    const/4 p1, 0x0\n"+
-						"    :lith_patch_hf_cont\n",
+						"    if-eqz v8, :lith_patch_hf_tyalive\n"+
+						"    goto :cond_3\n"+
+						"    :lith_patch_hf_tyalive\n",
+				),
+				// Force nav bar listener choice to invisibleAfter on show when enabled
+				ReplaceStringPrepend(
+					"    if-eqz p1, :cond_4\n",
+					"    # lithiumpatch: hide footer bar on show when enabled (listener)\n"+
+						"    const/4 v8, 0x0\n"+
+						"    iget-object v7, p0, Lcom/faultexception/reader/ReaderActivity;->mPrefs:Landroid/content/SharedPreferences;\n"+
+						"    const-string v6, \"hide_reader_footer\"\n"+
+						"    invoke-interface {v7, v6, v8}, Landroid/content/SharedPreferences;->getBoolean(Ljava/lang/String;Z)Z\n"+
+						"    move-result v8\n"+
+						"    if-eqz v8, :lith_patch_hf_lalive\n"+
+						"    goto :cond_4\n"+
+						"    :lith_patch_hf_lalive\n",
 				),
 			),
 		),
